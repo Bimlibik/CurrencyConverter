8f92039dd3704de34df56c5116016a34958c35a8
Factis <53155715qwerty>
2020-09-10 19:40:42 +0800

    renamed

--- app/src/main/java/com/foxy/currencyconverter/ui/CurrencyListActivity.kt
+++ app/src/main/java/com/foxy/currencyconverter/ui/CurrencyListActivity.kt
@@ -1,9 +1,10 @@
-package com.foxy.currencyconverter
+package com.foxy.currencyconverter.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.foxy.currencyconverter.R
 
-class MainActivity : AppCompatActivity() {
+class CurrencyListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
