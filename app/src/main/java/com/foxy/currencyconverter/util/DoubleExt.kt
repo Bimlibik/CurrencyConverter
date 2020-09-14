25ac635f8cc75aa31bf10544be98e94b522b1160
Factis <53155715qwerty>
2020-09-14 15:57:13 +0800

    added comment

--- app/src/main/java/com/foxy/currencyconverter/util/DoubleExt.kt
+++ app/src/main/java/com/foxy/currencyconverter/util/DoubleExt.kt
@@ -1,6 +1,10 @@
 package com.foxy.currencyconverter.util
 import kotlin.math.round
 
+/**
+ *  Extension that round a number to 2 decimals
+ */
+
 fun Double.round(): Double {
     var multiplier = 1.0
     repeat(2) { multiplier *= 10 }
